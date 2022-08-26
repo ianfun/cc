1f9881a8ece6767944344a8a505610f5aa5b7057
ianfun <015006@mst.hlis.hlc.edu.tw>
2022-08-26 17:16:18 +0800

    doc2

--- appInstance.nim
+++ appInstance.nim
@@ -2,7 +2,7 @@ type
     VerboseLevel* = enum
       WError, WWarning, WNote, WVerbose
 
-type CC = object
+type CC* = object
     optLevel*: cuint ## 0 = -O0, 1 = -O1, 2 = -O2, 3 = -O3
     sizeLevel*: cuint ## 0 = none, 1 = -Os, 2 = -Oz
     inlineThreshold*: cuint
