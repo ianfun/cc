6f20122be510f408ddb375a84e36c1027cd02344
ianfun <015006@mst.hlis.hlc.edu.tw>
2022-09-25 08:12:24 +0800

    rewrite

--- push.bat
+++
@@ -1,5 +0,0 @@
-nim check cc
-nim doc --project --index:on --git.url:https://github.com/ianfun/cc --git.commit:master cc.nim
-git add .
-git commit 
-git push
--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# g++ `llvm-config --cxxflags` llvm/llvmAPI.cpp  -fsyntax-only
-nim check cc
-nim doc --project --index:on --git.url:https://github.com/ianfun/cc --git.commit:master cc.nim
-git add .
-git commit 
-git push
