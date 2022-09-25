209a637155729de554ef9b333535f05571541e05
ianfun <015006@mst.hlis.hlc.edu.tw>
2022-09-25 11:23:11 +0800

    readline

--- cc.nim
+++ cc.nim
@@ -37,7 +37,7 @@ import std/[math, sets, tables, editdistance, heapqueue, sequtils, os, times, un
 import llvm/llvm
 
 when not defined(CC_NO_RAEADLINE):
-    import readLine/[readLine, history]
+    import readline/[readline, history]
 
 type 
     VerboseLevel = enum
